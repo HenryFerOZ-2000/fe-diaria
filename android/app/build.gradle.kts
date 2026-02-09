e2e7e87f34dde9d7391fbdf838f2b1192ce7ef69
Hugo Larrea <hlarrea@fi.uba.ar>
2026-02-09 16:42:14 -0600

    fix google sign in

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // Requerido para Google Mobile Ads
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
+        versionCode = 5
         versionName = flutter.versionName
     }
 
