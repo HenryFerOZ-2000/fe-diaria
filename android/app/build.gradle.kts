d10e7c697e13efa018d7d4203fde2ce5636bc94c
Hugo Larrea <hlarrea@fi.uba.ar>
2026-03-02 12:36:08 -0600

    version 1.0.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion  // Requerido para Google Mobile Ads
         targetSdk = flutter.targetSdkVersion
-        versionCode = 6
+        versionCode = 7
         versionName = flutter.versionName
     }
 
